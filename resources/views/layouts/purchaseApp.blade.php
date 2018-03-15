1049516111b87da489c684473e7f327a03ec7c91
Richard <richard_brons@hotmail.com>
2018-03-15 10:15:25 +0100

    Install Bulma via npm

--- resources/views/layouts/purchaseApp.blade.php
+++ resources/views/layouts/purchaseApp.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Purchase Demo App</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
     <link rel="stylesheet" type="text/css" href="/css/app.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>
   </head>
